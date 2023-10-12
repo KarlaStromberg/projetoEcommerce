9bdf85591ed459e0fe7ce45567cd2c32c015613d
karlaStromberg <karlastromberg@gmail.com>
2023-10-11 21:45:19 -0300

    Transparent header

--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -1,27 +1,50 @@
-import { Navbar, Nav, Container, NavbarCollapse, NavbarText} from "react-bootstrap";
-import { GerenciarLogin } from "./GerenciarLogin";
+import React, { useEffect, useState } from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { GerenciarLogin } from './GerenciarLogin';
 
 function NavBar() {
+  const [isScrolled, setIsScrolled] = useState(false);
 
-    return (
-        <>
-            <Navbar bg="dark" data-bs-theme="dark" fixed="top" className="bg-opacity-50">
-                <Container>
-                    <Navbar.Brand href="/">E-commerce</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/produtos">Produtos</Nav.Link>
-                    </Nav>
-                    <NavbarCollapse className="justify-content-end">
-                            <NavbarText>
-                                <GerenciarLogin/>
-                            </NavbarText>
-                        </NavbarCollapse>
-                </Container>
-            </Navbar>
-        </>
- );
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <>
+      <Navbar
+        className={isScrolled ? '' : 'bg-opacity-50'} 
+        data-bs-theme="dark"
+        fixed="top"
+        bg="dark"
+       
+      >
+        <Container>
+          <Navbar.Brand href="/">E-commerce</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/produtos">Produtos</Nav.Link>
+          </Nav>
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              <GerenciarLogin />
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
-
 
 export default NavBar;
